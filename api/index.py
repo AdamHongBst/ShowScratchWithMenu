ae2143842967fa3c00b54208c10166a09f92f712
AdamHong <129084590+AdamHongBst@users.noreply.github.com>
2023-08-17 11:53:17 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,21 +1,9 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
 
-class MyRequestHandler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        if self.path == "/":
-            self.send_response(302)  # 302状态码表示临时重定向
-            self.send_header("Location", "/ShowScratchWithMenu.html")  # 设置跳转的目标URL
-            self.end_headers()
-        else:
-            self.send_response(404)  # 返回404状态码表示页面不存在
-            self.end_headers()
-
-def run(server_class=HTTPServer, handler_class=MyRequestHandler, port=8000):
-    server_address = ("", port)
-    httpd = server_class(server_address, handler_class)
-    print(f"Starting server on port {port}...")
-    httpd.serve_forever()
-
-if __name__ == "__main__":
-    run()
-
+        self.send_response(302)  # 设置重定向状态码 302
+        self.send_header('Location', '/ShowScratchWithMenu.html')  # 设置重定向的目标路径
+        self.end_headers()
+        return
