5212bfe0b0c24078126eccec65e9ec1cb6fc1e5d
AdamHong <129084590+AdamHongBst@users.noreply.github.com>
2023-08-17 11:28:27 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    # 使用 redirect 函数重定向到根目录下的 ShowScratchWithMenu.html 文件
+    return redirect(url_for('static', filename='ShowScratchWithMenu.html'))
+
+if __name__ == "__main__":
+    app.run()
