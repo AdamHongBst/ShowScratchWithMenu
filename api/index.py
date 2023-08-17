d1ec9c7675f399cf26eebd3ad18d9baccea8848b
AdamHong <129084590+AdamHongBst@users.noreply.github.com>
2023-08-17 11:49:50 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,21 @@
-from flask import Flask, redirect, url_for
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-app = Flask(__name__)
+class MyRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(302)  # 302状态码表示临时重定向
+            self.send_header("Location", "/ShowScratchWithMenu.html")  # 设置跳转的目标URL
+            self.end_headers()
+        else:
+            self.send_response(404)  # 返回404状态码表示页面不存在
+            self.end_headers()
 
-@app.route("/api/python")
-def hello_world():
-    # 使用 redirect 函数重定向到根目录下的 ShowScratchWithMenu.html 文件
-    return redirect(url_for('static', filename='ShowScratchWithMenu.html'))
+def run(server_class=HTTPServer, handler_class=MyRequestHandler, port=8000):
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}...")
+    httpd.serve_forever()
 
 if __name__ == "__main__":
-    app.run()
+    run()
+
